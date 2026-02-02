060a88762befc48c0a9315f712f3e1d10e994051
Alexander <pchp250g8@tutamail.com>
2026-02-02 11:32:40 +0300

    Добавьте файлы проекта.

---
+++ DrawEllipse/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawEllipce
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.Black);
+            g.DrawEllipse(Pens.Yellow, 0, 0, ClientSize.Width, ClientSize.Height);
+            g.FillEllipse(Brushes.Red, 0, 0, ClientSize.Width, ClientSize.Height);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+    }
+}
